ae23f130fbf1eefbf938aa222c6893114a3f97b1
Ян Купчинский <yaks06@mail.ru>
2026-06-12 13:15:55 +0300

    stuff

---
+++ ПИ/2 курс 2025-2026/formating.py
@@ -0,0 +1,28 @@
+import os
+
+directory = '4 Семестр/КиТГр/'
+
+for filename in os.listdir(directory):
+    if filename.endswith('.md'):
+        filepath = os.path.join(directory, filename)
+
+        with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.read()
+
+        # Выполняем замены по правилам:
+        # /(  ->  $
+        # /)  ->  $
+        # /[  ->  $$
+        # /]  ->  $$
+        new_content = content.replace('\\( ', '$')
+        new_content = new_content.replace(' \\)', '$')
+        new_content = new_content.replace('\\(', '$')
+        new_content = new_content.replace('\\)', '$')
+        new_content = new_content.replace('\\[', '$$')
+        new_content = new_content.replace('\\]', '$$')
+
+        # Записываем обратно в тот же файл
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.write(new_content)
+
+        print(f'Обработан: {filename}')
